745ecb709d7ef067dbcb1657685c4cd154810c58
Lenscak José Francisco <jose.lenscak@globant.com>
2014-05-08 16:29:42 -0300

    Add basic interface

--- Tyapu-interface/tyapuinterface.h
+++ Tyapu-interface/tyapuinterface.h
@@ -2,6 +2,7 @@
 #define TYAPUINTERFACE_H
 
 #include "tyapu-interface_global.h"
+#include <QMediaPlayer>
 
 class TYAPUINTERFACESHARED_EXPORT Tyapuinterface
 {
@@ -9,6 +10,21 @@ class TYAPUINTERFACESHARED_EXPORT Tyapuinterface
 public:
     Tyapuinterface();
     virtual void addSong(QString &path) = 0;
+    virtual QList playList() = 0;
+    virtual QString actualSong() = 0;
+    virtual intb actualSongIndex() = 0;
+    virtual bool isMuted() = 0;
+    virtual MediaStatus mediaStatus() = 0;
+    virtual int volume() = 0;
+    virtual void setVolume(int value) = 0;
+    virtual void pause() = 0;
+    virtual void play() = 0;
+    virtual void mute() = 0;
+    virtual void unmute() = 0;
+    virtual void stop() = 0;
+    virtual void next() = 0;
+    virtual void previous() = 0;
+    virtual void selectSong() = 0;
 };
 
 #endif // TYAPUINTERFACE_H
