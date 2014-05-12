9ad674d27a635ba0b96011c025f52295ae871995
Lenscak José Francisco <jose.lenscak@globant.com>
2014-05-12 16:47:03 -0300

    Easter egg.

--- Tyapu-console/main.cpp
+++ Tyapu-console/main.cpp
@@ -1,8 +1,34 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <string>
+#include <sstream>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    std::cout << "Bienvenido a Tyapu, el mejor reproductor de música del NO mercado.\n";
 
-    return a.exec();
+    std::string command;
+
+    while(command != "salir"){
+        std::cin >> command;
+
+        if(command == "ayuda"){
+            std::cout << "agregar <ruta>               Agrega de manera recursiva los archivos de una ruta.\n";
+        }
+
+        if(command == "huevoDePascuas"){
+            std::cout << "Acá tenes tu huevo\n";
+            sleep(2);
+            std::cout << "******** @-@-@*********\n";
+            std::cout << "****** @————–—–@*******\n";
+            std::cout << "**** @————————––—@*****\n";
+            std::cout << "*** @——- Felices——@****\n";
+            std::cout << "** @——– Pascuas——–—@***\n";
+            std::cout << "** @——(\\(\\——————–——@***\n";
+            std::cout << "** @——(^.^)—–—————–@***\n";
+            std::cout << "** @—*(..(“)(“).———@***\n";
+            std::cout << "*** @—————————–———@****\n";
+            std::cout << "***** @———————–—@******\n";
+            std::cout << "******* @-@-@-@********\n";
+        }
+    }
 }
